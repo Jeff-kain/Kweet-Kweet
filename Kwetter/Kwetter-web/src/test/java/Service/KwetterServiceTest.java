dad309e47ea47278cb0a500e085b6eafe4c373a0
JeffKain <Heartless275>
2017-02-27 11:03:40 +0100

    Test commented

--- Kwetter/Kwetter-web/src/test/java/Service/KwetterServiceTest.java
+++ Kwetter/Kwetter-web/src/test/java/Service/KwetterServiceTest.java
@@ -1,55 +1,55 @@
-package Service;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-import Dao.KwetterDAO;
-import Service.KwetterService;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.mockito.Mock;
-
-/**
- *
- * @author jeffrey
- */
-public class KwetterServiceTest {
-
-    private KwetterService service;
-    @Mock
-    private KwetterDAO kwetterDAO;
-
-    public KwetterServiceTest() {
-        
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-        service = new KwetterService();
-        service.setKwetterDAO(kwetterDAO);
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
-}
+//package Service;
+//
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//import Dao.KwetterDAO;
+//import Service.KwetterService;
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//import org.mockito.Mock;
+//
+///**
+// *
+// * @author jeffrey
+// */
+//public class KwetterServiceTest {
+//
+//    private KwetterService service;
+//    @Mock
+//    private KwetterDAO kwetterDAO;
+//
+//    public KwetterServiceTest() {
+//        
+//    }
+//
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//
+//    @Before
+//    public void setUp() {
+//        service = new KwetterService();
+//        service.setKwetterDAO(kwetterDAO);
+//    }
+//
+//    @After
+//    public void tearDown() {
+//    }
+//
+//    // TODO add test methods here.
+//    // The methods must be annotated with annotation @Test. For example:
+//    //
+//    // @Test
+//    // public void hello() {}
+//}
